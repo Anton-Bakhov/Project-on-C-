a048e837d8044db7380fcc29af0bc037eb646008
Anton-Bakhov <45654039+Anton-Bakhov@users.noreply.github.com>
2021-04-27 01:26:26 +0300

    Update ShopProductInfo.cpp

--- ShopProductInfo.cpp
+++ ShopProductInfo.cpp
@@ -18,7 +18,7 @@ void ShopProductInfo::printAllItemInfo(product& prod,Item& item)
 		out <<"<h2> "<<"'" <<" Your Bill "<< "'"<< " </h2> "<<"\n"<< "<p>" <<"Price of product: " <<prod.endPrice() <<"$ "<< "<br>"<<" \n"<< "Product #: " << item.GetNumbOfProduct()<< "<br>" << "\n"
 			<< "Name of Product: " << item.GetNameOfProduct() << "\n"<<"</p>" << std::endl;
 	
-		out << " </html> " << "\n" << "</body>" << "\n";
+		out << "</body>"<< "\n" << " </html> "   << "\n";
 	}
 	out.close();
 }
